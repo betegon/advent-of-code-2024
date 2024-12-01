589bdd451052ae55e43bccbcad25d6ab1168b2fd
betegon <miguelbetegongarcia@gmail.com>
2024-12-01 22:00:38 +0100

    soolution

--- 1/2.py
+++ 1/2.py
@@ -3,7 +3,8 @@ from utils import read_input
 input = read_input()
 left = [int(n[0]) for n in input]
 right = [int(n[1]) for n in input]
-left.sort()
-right.sort()
-distance = sum([abs(x - y) for x, y in zip(left, right)])
-a = zip(left, right)
+similarity = 0
+for idx, n in enumerate(left):
+    similarity += right.count(n)*n
+
+print(similarity)
