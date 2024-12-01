865843081a6282960fdaced97e731b61160d48fd
betegon <miguelbetegongarcia@gmail.com>
2024-12-01 22:31:06 +0100

    optimize solution

--- 1/2.py
+++ 1/2.py
@@ -1,10 +1,9 @@
 from utils import read_input
+from collections import Counter
 
 input = read_input()
 left = [int(n[0]) for n in input]
 right = [int(n[1]) for n in input]
-similarity = 0
-for idx, n in enumerate(left):
-    similarity += right.count(n)*n
-
+right_counts = Counter(right)
+similarity = sum(n*right_counts.get(n, 0) for n in left)
 print(similarity) # solution: 24941624
