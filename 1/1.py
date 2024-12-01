5a1d295da3df840fe4d387d83ca478314bee0037
betegon <miguelbetegongarcia@gmail.com>
2024-12-01 22:30:56 +0100

    optimize solution

--- 1/1.py
+++ 1/1.py
@@ -3,7 +3,5 @@ from utils import read_input
 input = read_input()
 left = [int(n[0]) for n in input]
 right = [int(n[1]) for n in input]
-left.sort()
-right.sort()
-distance = sum([abs(x - y) for x, y in zip(left, right)])
+distance = sum([abs(x - y) for x, y in zip(sorted(left), sorted(right))])
 print(distance) # solution: 2086478 
