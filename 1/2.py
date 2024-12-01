3e8112795dd219d5a20e2736078c4f1e6ce07249
betegon <miguelbetegongarcia@gmail.com>
2024-12-01 21:55:33 +0100

    start from 1

---
+++ 1/2.py
@@ -0,0 +1,9 @@
+from utils import read_input
+
+input = read_input()
+left = [int(n[0]) for n in input]
+right = [int(n[1]) for n in input]
+left.sort()
+right.sort()
+distance = sum([abs(x - y) for x, y in zip(left, right)])
+a = zip(left, right)
